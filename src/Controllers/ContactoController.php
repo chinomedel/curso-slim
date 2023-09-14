f29091a6238aaba636fc197a12aa9de9f741f276
Nelson Medel <nmedelb@gmail.com>
2023-09-14 14:25:57 -0300

    Creado templates básicos con blocks de twig

--- src/Controllers/ContactoController.php
+++ src/Controllers/ContactoController.php
@@ -3,17 +3,18 @@
 namespace App\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig; //llamo a la clase Twig
 
 class ContactoController{
 
     function index($request, $response, $args){
-        $view = Twig::fromRequest($req);
+        $view = Twig::fromRequest($request);
        // En el array parámetros le envío datos al html en este caso al index.html
        // En el index.html le mandé el title y lo llamo en tre docles llaves {{}}
        $parametros = [
-        "title"=>"Página principal",
-        "categoria"=>"home"
+        "title"=>"Contacto",
+        "categoria"=>"contacto"
        ];
-        return $view->render($res, "index.html", $parametros);
+        return $view->render($response, "contacto.html", $parametros);
     }
 }
